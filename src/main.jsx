16de9cbf6c490ce23af5119667b499b3cb4feddf
nucleophile007 <141562552+nucleophile007@users.noreply.github.com>
2024-11-18 09:04:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "https://e-learning-server-hah3.onrender.com/";
+export const server = "https://e-learning-server-hah3.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
